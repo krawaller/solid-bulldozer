411bfbffe6a096a85923ed00afb36c5bdb6ec411
Haris <haris.kljajic@hotmail.se>
2013-11-23 21:31:59 +0100

    Added structure

---
+++ Solid Bulldozer/app.js
@@ -0,0 +1,9 @@
+//The main application!
+//This is the main in the application. It will call and startup backbone etc
+define(["backbone"],function(Backbone){
+	return {
+		start: function(){
+			console.log("...START CALL!");
+		}
+	};
+});
