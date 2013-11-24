cf2edd8aecaefbb28407a2785f31a739ce7ada5c
Haris <haris.kljajic@hotmail.se>
2013-11-24 16:51:47 -0500

    Fixed error in bootstrapJS

--- config.js
+++ config.js
@@ -3,9 +3,10 @@ console.log("...LOADING config.js");
 //This  file is a config for all the dependencies needed in the project.
 require.config({
 	paths: {
-		jquery: "lib/jquery/jquery",
+		jQuery: "lib/jquery/jquery",
 		underscore: "lib/underscore/underscore",
 		backbone: "lib/backbone/backbone",
+		bootstrap: "lib/bootstrap/bootstrap",
 		jade: "lib/jade/jade",
 		"bb-rel":"lib/backbone/backbone-relational",
         "bb-loc":"lib/backbone/backbone.localStorage.async",
@@ -15,7 +16,7 @@ require.config({
         exports: '_'
     },
     backbone: {
-      deps: ['underscore', 'jquery'],
+      deps: ['underscore', 'jQuery'],
       exports: 'Backbone'
     }
   }
