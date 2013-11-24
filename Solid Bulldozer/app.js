f8f807834220c09a1a6688dece6463c42de6edbf
Haris <haris.kljajic@hotmail.se>
2013-11-24 22:30:32 +0100

    fel

--- Solid Bulldozer/app.js
+++
@@ -1,9 +0,0 @@
-//The main application!
-//This is the main in the application. It will call and startup backbone etc
-define(["backbone"],function(Backbone){
-	return {
-		start: function(){
-			console.log("...START CALL!");
-		}
-	};
-});
